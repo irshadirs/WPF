8cbd9c05f1f22096502fe0e6a42735e1dad073bf
Irshad <Irshadpp.pp@gmail.com>
2021-12-21 23:22:21 +0530

    first commit

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Views;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<Services.ICustomerStore, Services.DbCustomerStore>();
+            // register other needed services here
+        }
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+    }
+}
---
+++ WpfApp1/Services.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    class Services
+    {
+        public interface ICustomerStore
+        {
+            List<string> GetAll();
+        }
+
+        public class DbCustomerStore : ICustomerStore
+        {
+            public List<string> GetAll()
+            {
+                return new List<string>()
+            {
+                "cust 1",
+                "cust 2",
+                "cust 3",
+            };
+            }
+        }
+    }
+
+}
